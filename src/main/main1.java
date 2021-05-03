3b4025c849560fc253919bd526a41d1c932410d6
JonatasPiassa <jo.piassa@hotmial.com>
2021-05-03 19:43:53 -0300

    wip

---
+++ src/main/main1.java
@@ -0,0 +1,42 @@
+package main;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class main1 extends StateBasedGame {
+
+	public main1(String name)
+	{
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[]args) {
+		AppGameContainer appgc;
+		
+		try {
+			appgc = new AppGameContainer(new Main ("Altheryn RPG"));
+			appgc.setDisplayMode(1366, 768, false);
+			appgc.setAlwaysRender(true);
+			appgc.start();
+			
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+	}
+	
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		
+		addState(new BasicGame());
+	}
+
+}
+
